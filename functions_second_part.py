20634618a81360ca9ad0ee6424ca8cb6dd28b5a7
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2020-12-07 16:03:50 +0100

    day1 - finish

---
+++ functions_second_part.py
@@ -0,0 +1,43 @@
+def print_person(name, surname, age=0):
+    print("name:", name, "sn:", surname, "age:", age)
+
+print_person("Jan", "Kowalski", 24)
+print_person("Greg", "Newborn")
+print_person(name="Leszek", age=43, surname="Tarkowski")
+
+def almost_sort(key=None, reverse=False):
+    pass
+
+almost_sort(reverse=True)
+almost_sort(None, True)
+almost_sort(key=len)
+
+def add(a, b):
+    return a + b
+
+# def add(a, b, c):
+#     return a + b + c
+
+print(add(3, 4))
+
+def many_args(a, b, *c):
+    print(a, b, c)
+
+#many_args(3)
+many_args(3, 4)
+many_args(3, 4, 5)
+many_args(3, 4, 5, 6)
+
+# summae(1,2,3,4,5)  -> return 15
+# summae(1,2,3)  -> return 6
+# summae()  -> not working
+# summae(2)  -> return 2
+
+def summae(first, *args):
+    for n in args:
+        first += n
+    return first
+
+print("------")
+print(summae(2))
+print(summae(1,2,3,4,5))
--- hosts.py
+++ hosts.py
@@ -1,7 +1,15 @@
 content = open("hosts.txt").read()
 print(content)
 
+for line in content.splitlines():
+    both_names, domain = line.split("@")
+    name, surname = both_names.split(".")
+    print("{:15} {:15} {}".format(name, surname, domain))
+
 ### print something like that:
 ## Jan        Nowak        apollo.pl
 ## Wanda      Kowalski     mrr.gov.pl
 ####
+## Bonus - use RegExp
+## import re
+## re.match
