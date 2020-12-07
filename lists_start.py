3cbe81b269e77d9df1a192983c83b79f51b923e9
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2020-12-07 13:08:47 +0100

    day1 - lists

---
+++ lists_start.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+name_list = ['Dionizy', 'Natalia', 'anna', 'Artur', 'Stefan', 'Kinga', 'Aleksandra', 
+           'Katarzyna', 'Zofia', 'Ksawery', 'Maciej', 'monika', 'Tomasz', 'Weronika',
+           'Marcin', 'Dominik', 'Ewa']
+
+foreign_name = ['Jean', 'Paul', 'Mao', 'Aurielie']
+
+# 1 - wyswietl z name_list imiona kobiet
+
+# 2 - usun z name_list Artura i Stefana
+
+# 3 - utworz liste imion zaczynajacych sie na M
+
+# 4 - posortuj alfabetycznie polaczona liste imion
+
+# 5 - posortuj poloczona liste wg dlugosci imion
+
+# 6 - odwroc kolejnosc posortowanej listy
