42bfcdf034cf158212d41d73f303da3d4fe57e71
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2020-12-09 14:07:13 +0100

    vector

--- test_vector.py
+++ test_vector.py
@@ -29,16 +29,50 @@ def test_check_vector_coords():
 
 def test_check_vector_length():
     vec = Vector(1, 1)
-    assert vec.length() == pytest.approx(1.4142, 0.001)
+    assert vec.length == pytest.approx(1.4142, 0.001)
     ## should optimize for math.sqrt called only once per coordinates changes
+
+def test_length_is_readonly():
+    vec = Vector(1, 1)
+    with pytest.raises(AttributeError):
+        vec.length = 123
 
 def test_vector_position_change():
     vec = Vector(1, 1)
-    vec.length()
-    vec.set_x(2)
-    vec.set_y(2)
-    assert vec.length() == pytest.approx(2.8284, 0.001)
+    vec.length
+    vec.x = 2
+    vec.y = 2
+    assert vec.length == pytest.approx(2.8284, 0.001)
 
+def test_adding_vectors():
+    v1 = Vector(2, 3)
+    v2 = Vector(3, 4)
+    v3 = v1 + v2    
+    assert v3.x == 5
+    assert v3.y == 7
+    assert v3.length == pytest.approx(8.602, 0.001)
+
+def test_adding_in_place_vectors():
+    v1 = Vector(2, 3)
+    v2 = Vector(3, 4)
+    v1 += v2    
+    assert v1.x == 5
+    assert v1.y == 7
+    assert v1.length == pytest.approx(8.602, 0.001)
+
+def test_geting_item():
+    vec = Vector(2, 3)
+    assert vec[0] == 2
+    assert vec[1] == 3
+    with pytest.raises(IndexError):
+        vec[3] == None
+
+def test_seting_item():
+    vec = Vector(1, 1)
+    vec[0] = 2
+    vec[1] = 3
+    with pytest.raises(IndexError):
+        vec[3] == None
 
     # import math
     # math.sqrt(x*x + y*y)
--- vector.py
+++ vector.py
@@ -4,26 +4,61 @@ class Vector:
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
-        self.__length = None
+        self.__len = None
 
-    def get_x(self):
-        return self.__x
-
-    def get_y(self):
-        return self.__y
-
-    def set_x(self, val):    
-        self.__length = None    
+    def __set_x(self, val):    
+        self.__len = None    
         self.__x = val
 
-    def set_y(self, val):
-        self.__length = None
+    def __set_y(self, val):
+        self.__len = None
         self.__y = val
 
     def __calculate_length(self):
         return m.sqrt(self.__x ** 2 + self.__y ** 2)
 
-    def length(self):
-        if self.__length == None:
-            self.__length = self.__calculate_length()
-        return self.__length
+    def __length(self):
+        if self.__len == None:
+            self.__len = self.__calculate_length()
+        return self.__len
+
+    def __add__(self, other):
+        result = Vector(self.x + other.x, self.y + other.y)
+        return result
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __getitem__(self, i):
+        if i == 0:
+            return self.x
+        elif i == 1:
+            return self.y
+        else:
+            raise IndexError            
+
+    def __setitem__(self, i, value):
+        if i == 0:
+            self.x = value
+        elif i == 1:
+            self.y = value
+        else:
+            raise IndexError
+
+    def __repr__(self):
+        return "Vector({}, {})".format(self.x, self.y)
+
+    # def __str__(self):
+    #     return "Vector at {}, {}".format(self.x, self.y)
+
+    x = property(fset=__set_x, fget=lambda self : self.__x)
+    y = property(fset=__set_y, fget=lambda self : self.__y)
+    length = property(fget=__length)  # read-only property
+
+v = Vector(1, 2)
+print(v)
+print(v.__repr__())
+
+    
